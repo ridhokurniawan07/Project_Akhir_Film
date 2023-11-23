b8b35d55f4827c2a41baf8c3554254d464587165
Muhammad Fachri Nuriza <fachri.nuriza@sg-dsa.com>
2023-11-23 16:37:48 +0700

    Create genre page and Components

---
+++ components/layer/header_page.php
@@ -0,0 +1,16 @@
+<!-- BEGIN | Header -->
+<header class="ht-header">
+    <div class="container">
+        <?php include_once'./components/layer/navbar.php'; ?>
+
+        <!-- top search form -->
+        <div class="top-search">
+            <select>
+            <option value="united">TV show</option>
+            <option value="saab">Others</option>
+            </select>
+            <input type="text" placeholder="Search for a movie, TV Show or celebrity that you are looking for" />
+        </div>
+    </div>
+</header>
+<!-- END | Header -->
