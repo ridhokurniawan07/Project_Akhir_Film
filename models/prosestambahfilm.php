d3085ff26e68eb884ba4211670b181ca553042f7
Mardhotillah12 <mardhotillah551@gmail.com>
2023-12-01 02:11:10 +0700

    fixing error

--- models/prosestambahfilm.php
+++ models/prosestambahfilm.php
@@ -10,30 +10,53 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $actor_ids = $_POST["actor_id"]; // Ini akan menjadi array karena menggunakan multiple="multiple"
     $film_image = $_FILES["film_image"]["name"];
 
-    // Target direktori untuk menyimpan gambar
-    $target_dir = "images/";
+    //menampung data file yang diupload
+    $filename = $_FILES['film_image']['name'];
+    $tmp_name = $_FILES['film_image']['tmp_name'];
 
-    // Upload gambar ke server
-    $target_file = $target_dir . basename($_FILES["film_image"]["name"]);
-    move_uploaded_file($_FILES["film_image"]["tmp_name"], $target_file);
+    $type1 = explode('.', $filename);
+    $type2 = $type1[1];
 
-    // Masukkan data ke dalam tabel tb_film
-    $query_insert_film = "INSERT INTO tb_film (genre_id, film_name, film_release, film_description, film_image) 
-                    VALUES ('$genre_id', '$film_name', '$film_release', '$film_description', '$film_image')";
+    $newname = 'film'.time().'.'.$type2;
 
-    if (mysqli_query($conn, $query_insert_film)) {
+    //menampung data format file yang diizinkan
+    $tipe_diizinkan = array('jpg', 'jpeg', 'png', 'gif');
+
+    //validasi format file
+    if(!in_array($type2, $tipe_diizinkan)){
+    //jika format file tidak ada di dalam tipe diizinkan
+    echo '<script>alert("Format file tidak diizinkan")</script>';
+
+    }else{
+     //jika format file sesuai dengan yang ada di dalam array tipe diizinkan
+    //proses upload file sekaligus insert ke database
+    $upload = move_uploaded_file($tmp_name, './images/film/' .$newname);
+    var_dump($upload);
+    exit;
+    if ($upload) {
+        // Masukkan data ke dalam tabel tb_film
+        $query_insert_film = "INSERT INTO tb_film (genre_id, film_name, film_release, film_description, film_image) 
+            VALUES ('$genre_id', '$film_name', '$film_release', '$film_description', '$newname')";
+
+        if (mysqli_query($conn, $query_insert_film)) {
         // Ambil ID film yang baru saja dimasukkan
         $last_inserted_film_id = mysqli_insert_id($conn);
 
         // Masukkan data aktor ke dalam tabel relasi tb_film_actor
         foreach ($actor_ids as $actor_id) {
-            mysqli_query($conn, "INSERT INTO tb_film_actor (film_id, actor_id) VALUES ('$last_inserted_film_id', '$actor_id')");
+        mysqli_query($conn, "INSERT INTO tb_film_actor (film_id, actor_id) VALUES ('$last_inserted_film_id', '$actor_id')");
         }
 
         echo "Film berhasil ditambahkan.";
+        } else {
+        echo "Error: " . $query_insert_film . "<br>" . mysqli_error($conn);
+        }
     } else {
         echo "Error: " . $query_insert_film . "<br>" . mysqli_error($conn);
     }
+}
+
+    
 }
 
 // Tutup koneksi
